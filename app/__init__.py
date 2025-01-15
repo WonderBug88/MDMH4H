f5931ed5542fe1343f2dfed1edec21e022ff9709
kishan <kishanchauhan00007@gmail.com>
2025-01-15 12:43:24 +0530

    ganesh scripts schedule

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.config import Config, load_config
 from etl_scripts.gsc.gsc_weekly_update import gsc_weekly_update_main
+from etl_scripts.ganeshmills.deskgmail_latest import start_latest_process
+from etl_scripts.ganeshmills.bigapi import big_api_main 
 from app.utilities.custom_filters import format_images, remove_none_str, round_number
 
 logging.basicConfig(level=logging.INFO, filename='app.log',
@@ -43,6 +45,14 @@ def create_app(config_class=load_config[Config.FLASK_ENV]):
 
     # Schedule the job to run every minute for testing
     # scheduler.add_job(func=gsc_weekly_update_main, trigger='interval', minutes=1)
+
+    # Schedule a job to run the process for downloading the latest Gmail attachment.
+    # This job will execute every Sunday at 11:00 PM.
+    scheduler.add_job(func=start_latest_process, trigger='cron', day_of_week='sun', hour=23, minute=0)
+
+    # Schedule a job to run the BigCommerce API function for inventory updates.
+    # This job will execute every Sunday at 11:30 PM.
+    scheduler.add_job(func=big_api_main, trigger='cron', day_of_week='sun', hour=23, minute=30)
 
     scheduler.start()
 
