39939059983d24568f5d16c8f8d1260f6a853a98
WonderBug88 <Support@hotels4humanity.com>
2026-05-01 13:14:51 -0700

    Trim non-runtime files from Route Authority deploy

--- tmp_start_5087_flask.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\juddu\Downloads\PAM\MDMH4H
-C:\Users\juddu\AppData\Local\Programs\Python\Python312\python.exe .\tmp_run_fulcrum_flask_5087.py
--- tmp_start_fulcrum_5087.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\juddu\Downloads\PAM\MDMH4H
-set FULCRUM_ENV_PATH=C:\Users\juddu\Downloads\PAM\fulcrum.alpha.env
-set ENABLE_SCHEDULER=0
-set FULCRUM_HOST=127.0.0.1
-set FULCRUM_PORT=5087
-set FLASK_ENV=development
-C:\Users\juddu\AppData\Local\Programs\Python\Python312\python.exe run_fulcrum_alpha.py >> C:\Users\juddu\Downloads\PAM\MDMH4H\tmp_fulcrum_5087_cmd.log 2>&1
